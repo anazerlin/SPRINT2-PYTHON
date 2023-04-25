e99caedb0ee09d7412948c645429bb666431ab06
anazerlin <rm98065@fiap.com.br>
2023-04-25 14:27:42 -0300

    iniciando estrutura

---
+++ index.py
@@ -0,0 +1,22 @@
+#INTRODUÇÃO DO PROGRAMA - Armazenando respostas com inputs 
+
+print("Seja bem vindo a nossa plataforma! Deseja fazer o seu cadastro?")
+respostaCadastro = input()
+if respostaCadastro != "nao":
+    print("Para fazer o seu cadastro, insira os seus dados para prosseguir.\nDigite seu novo usuario")
+    nomeUsuario = input()
+    print("Agora, digite a sua nova senha numerica.")
+    senhaUsuario = int(input())
+else:
+    print("Ok! Programa encerrado!")
+
+
+
+
+
+# if 
+# print(f"Seja bem vindo ao seu perfil, {nomeUsuario}.")
+
+
+#CONSTRUINDO UMA FUNÇÃO DE MENU COM LOOPS:
+
